fe787ed5f22fbf42a38a4f6cd0396de7dd61e7a2
Robin Herbots <robin.herbots@gmail.com>
2013-10-29 10:05:47 +0100

    optimize ui-timepicker

--- GlobalSauce/Rendertargets/JqueryUITimePickerRenderer.cs
+++ GlobalSauce/Rendertargets/JqueryUITimePickerRenderer.cs
@@ -20,7 +20,7 @@ namespace GlobalSauce.Rendertargets
             timepickerCulture.AppendFormat("timezoneText: '{0}',\n", Resources.JqueryUITimePicker.timezoneText);
             timepickerCulture.AppendFormat("currentText: '{0}',\n", Resources.JqueryUITimePicker.currentText);
             timepickerCulture.AppendFormat("closeText: '{0}',\n", Resources.JqueryUITimePicker.closeText);
-            timepickerCulture.AppendFormat("timeFormat: '{0}',\n", cultureInfo.DateTimeFormat.JqueryUITimeFormat());
+            timepickerCulture.AppendFormat("timeFormat: '{0}',\n", cultureInfo.DateTimeFormat.ShortTimePattern);
             timepickerCulture.AppendFormat("amNames: ['{0}'],\n", cultureInfo.TextInfo.ToUpper(cultureInfo.DateTimeFormat.AMDesignator));
             timepickerCulture.AppendFormat("pmNames: ['{0}'],\n", cultureInfo.TextInfo.ToUpper(cultureInfo.DateTimeFormat.PMDesignator));
             timepickerCulture.AppendFormat("isRTL: {0}\n", cultureInfo.TextInfo.IsRightToLeft.ToString().ToLowerInvariant());
@@ -28,17 +28,6 @@ namespace GlobalSauce.Rendertargets
             timepickerCulture.AppendFormat("$.timepicker.setDefaults($.timepicker.regional['{0}']);\n", cultureInfo.Name);
 
             return timepickerCulture.ToString();
-        }
-    }
-
-    public static class TimeFormatInfoExtensions
-    {
-        public static string JqueryUITimeFormat(this DateTimeFormatInfo dateTimeFormatInfo)
-        {
-            var tfs = dateTimeFormatInfo.ShortTimePattern.Split(Convert.ToChar(dateTimeFormatInfo.TimeSeparator));
-            var minSplit = tfs[1].Split(' ');
-
-            return string.Format("{0}{0}{2}{1}{1}{3}", tfs[0][0].ToString().ToUpperInvariant(), minSplit[0][0], dateTimeFormatInfo.TimeSeparator, minSplit.Length > 1 ? " " + minSplit[1][0] : string.Empty);
         }
     }
 }
