040526596bd9e71a03edc3c4d50634db87f328ad
Robin Herbots <robin.herbots@gmail.com>
2016-03-14 14:33:43 +0100

    Globalsauce 0.2.3

--- GlobalSauce/GlobalSauceController.cs
+++ GlobalSauce/GlobalSauceController.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,6 +14,15 @@ namespace GlobalSauce
         public virtual JavaScriptResult Get(Components id)
         {
             return JavaScript(RenderTargetProcessor.Render(id));
+        }
+        [HttpGet]
+        public virtual JavaScriptResult Get(string id)
+        {
+            Components component;
+            if (Enum.TryParse(id.Remove(id.Length - ".js".Length), out component))
+                return JavaScript(RenderTargetProcessor.Render(component));
+
+            return new JavaScriptResult();
         }
     }
 }
